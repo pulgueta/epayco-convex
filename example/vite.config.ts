e4a7c3e2820627d90448b6e29fb8d60456c86a56
pulgueta <retardix456@gmail.com>
2026-07-07 14:53:06 -0500

    feat: add nitro deployment

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,13 +5,15 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    nitro(),
     devtools(),
     tailwindcss(),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter(),
     viteReact(),
   ],
 })
