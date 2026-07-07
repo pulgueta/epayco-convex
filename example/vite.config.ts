5fec9077770c778fbd685f5f19d823cd041d33d2
pulgueta <retardix456@gmail.com>
2026-07-07 15:08:46 -0500

    fix(example): configure vercel static build

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,12 +5,10 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
-    nitro(),
     devtools(),
     tailwindcss(),
     tanstackRouter(),
